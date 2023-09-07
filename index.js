5b1b7f5102e0bb3664f47debaf0e0ec80e67b677
RobinJesba <jb.robinm@gmail.com>
2023-09-07 14:26:44 +0530

    changes to add and remove labels in PR

---
+++ index.js
@@ -0,0 +1,66 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function updateLabelInPR() {
+  try {
+    const eventName = github.context.eventName;
+    if (eventName !== "pull_request")
+      throw new Error(
+        `This action is intended to run only on pull_request events, not on ${eventName} events.`
+      );
+
+    const labelsToAdd = [...new Set(core.getInput("LABELS_TO_ADD").split(","))];
+    const labelsToRemove = [
+      ...new Set(core.getInput("LABELS_TO_REMOVE").split(",")),
+    ];
+    if (!labelsToAdd.length && !labelsToRemove.length)
+      throw new Error(
+        "labelsToAdd, labelsToRemove atleast either one is required!"
+      );
+
+    const token = core.getInput("GITHUB_TOKEN");
+    const octokit = github.getOctokit({ auth: token });
+    const owner = github.context.payload.repository.owner.login;
+    const repo = github.context.payload.pull_request.base.repo.name;
+    const pullRequestNumber = github.context.payload.pull_request.number;
+
+    const parameters = {
+      owner,
+      repo,
+      issue_number: pullRequestNumber,
+    };
+
+    let response;
+    if (labelsToAdd.length === 1 || labelsToRemove.length === 1) {
+      if (labelsToAdd.length)
+        response = await octokit.rest.issues.addLabels({
+          ...parameters,
+          name: labelsToAdd[0],
+        });
+      else
+        response = await octokit.rest.issues.removeLabels({
+          ...parameters,
+          name: labelsToRemove[0],
+        });
+    } else {
+      const listAllLabelsResponse = await octokit.rest.issues.listLabelsOnIssue(
+        {
+          ...parameters,
+        }
+      );
+      const updatedLabels = listAllLabelsResponse.data
+        .map((label) => label.name)
+        .filter((label) => !labelsToRemove.includes(label))
+        .concat(labelsToAdd);
+      response = await octokit.rest.issues.setLabels({
+        ...parameters,
+        labels: updatedLabels,
+      });
+    }
+    core.info(response);
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
+
+updateLabelInPR();
