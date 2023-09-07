d4b3ce877cce3ef96e0ee8e875b87c1ec1d89d4e
RobinJesba <jb.robinm@gmail.com>
2023-09-07 20:13:48 +0530

    test

--- index.js
+++ index.js
@@ -9,14 +9,11 @@ async function updateLabelInPR() {
         `This action is intended to run only on pull_request events, not on ${eventName} events.`
       );
 
-      console.log(core.getInput("LABELS_TO_ADD"));
-      console.log(core.getInput("LABELS_TO_REMOVE"));
     const labelsToAdd = [...new Set(core.getInput("LABELS_TO_ADD").split(","))];
     const labelsToRemove = [
       ...new Set(core.getInput("LABELS_TO_REMOVE").split(",")),
     ];
-    console.log(labelsToAdd);
-    console.log(labelsToRemove);
+
     if (!labelsToAdd.length && !labelsToRemove.length)
       throw new Error(
         "labelsToAdd, labelsToRemove atleast either one is required!"
@@ -34,32 +31,24 @@ async function updateLabelInPR() {
     };
 
     let response;
-    if (labelsToAdd.length === 1 || labelsToRemove.length === 1) {
-        console.log('if block');
-        console.log(labelsToAdd.length);
-        console.log(labelsToAdd[0]);
-        console.log({
-            ...parameters,
-            name: labelsToAdd[0],
-        });
-      if (labelsToAdd.length)
+    if ((labelsToAdd.length === 1 && !labelsToRemove.length) || 
+        (labelsToRemove.length === 1 || !labelsToAdd.length)) {
+      if (labelsToAdd.length && labelsToAdd[0])
         response = await octokit.rest.issues.addLabels({
           ...parameters,
           labels: labelsToAdd,
         });
-      else
+      else if (labelsToRemove[0])
         response = await octokit.rest.issues.removeLabels({
           ...parameters,
           labels: labelsToRemove,
         });
     } else {
-        console.log('else block')
       const listAllLabelsResponse = await octokit.rest.issues.listLabelsOnIssue(
         {
           ...parameters,
         }
       );
-      console.log(listAllLabelsResponse)
       const updatedLabels = listAllLabelsResponse.data
         .map((label) => label.name)
         .filter((label) => !labelsToRemove.includes(label))
